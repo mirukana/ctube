65d4b41d6ffc0dc36524c8221666865310da6143
miruka <miruka@disroot.org>
2020-11-18 18:05:11 -0400

    Create src/static/css dir if it doesn't exist

--- reload-css.sh
+++ reload-css.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 set -e
+
+mkdir -p src/static/css
 echo src/styles/main.sass |
     entr -nr sassc src/styles/main.sass src/static/css/main.css
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 set -e
+
 cd src
+mkdir -p static/css
 sassc styles/main.sass static/css/main.css
 uvicorn main:app --reload
