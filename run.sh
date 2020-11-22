d90fb4603da7e7dde95b88a504a1f9213b747e19
miruka <miruka@disroot.org>
2020-11-22 06:51:03 -0400

    run.sh: don't fail if there's no uvicorn to kill

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ mkdir -p ything/static/css || exit 1
 while true; do
     find ything -type f -not -name 'main.css' -not -path '*__pycache__*' |
     entr -cdnr sh -c \
-        "sassc ything/styles/main.sass ything/static/css/main.css && sleep 0.2 && killall -9 uvicorn && uvicorn ything:APP $*"
+        "sassc ything/styles/main.sass ything/static/css/main.css && sleep 0.2 && killall -9 uvicorn; uvicorn ything:APP $*"
 done
